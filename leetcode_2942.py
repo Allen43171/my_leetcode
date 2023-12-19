3c2d98b6f6d016c101b57c570ce200db03f38fc6
Allen43171 <hukirsa236875@gmail.com>
2023-12-19 17:07:07 +0800

    Add new file: leetcode_2942.py

---
+++ leetcode_2942.py
@@ -0,0 +1,10 @@
+# 2942 Find Words Containing Character
+# string
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        ans = []
+        for i in range(len(words)):
+            if x in words[i]:
+                ans.append(i)
+
+        return (ans)
