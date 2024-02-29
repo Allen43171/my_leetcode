483eb18d7020b586129a9f0c1dd50d58dc59f627
Allen43171 <hukirsa236875@example.com>
2024-02-29 15:51:11 +0800

    Update file: leetcode_2161.py, add some commit

--- leetcode_2161.py
+++ leetcode_2161.py
@@ -1,9 +1,12 @@
 # 2161. Partition Array According to Given Pivot
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        # create 3 empty list
         res_less = []
         res_equal = []
         res_more = []
+
+        # for loop to put element in those lists
         for ele in nums:
             if ele < pivot:
                 res_less.append(ele)
